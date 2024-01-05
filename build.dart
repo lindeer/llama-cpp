ea8c5aa34daa06eedfb0f690b66f5bff95b46568
lindeer <cljs118@126.com>
2024-01-05 19:54:23 +0800

    downgrade dart version to make project work

--- build.dart
+++ build.dart
@@ -5,7 +5,6 @@
 import 'dart:io' show File, Platform, Process, exit, stdout;
 import 'package:path/path.dart' as p;
 import 'package:native_assets_cli/native_assets_cli.dart';
-import 'package:ffigen/ffigen.dart' as fg;
 
 const packageName = 'llama_cpp';
 const _repoLibName = 'libllama.so';
@@ -15,13 +14,10 @@ const _repoLibName = 'libllama.so';
 void main(List<String> args) async {
   // Parse the build configuration passed to this CLI from Dart or Flutter.
   final buildConfig = await BuildConfig.fromArgs(args);
-  final ffiConfig = fg.Config.fromFile(
-    File.fromUri(buildConfig.packageRoot.resolve('ffigen.yaml')));
-  final ffiLib = fg.parse(ffiConfig);
-  ffiLib.generateFile(File.fromUri(Uri.parse(ffiConfig.output)));
 
   final env = Platform.environment;
   final cublas = env['LLAMA_CUBLAS'];
+  final srcDir = buildConfig.packageRoot.resolve('src');
   final proResult = await Process.run(
     'make',
     [
@@ -29,7 +25,7 @@ void main(List<String> args) async {
       if (cublas != null)
         'LLAMA_CUBLAS=$cublas',
     ],
-    workingDirectory: 'src',
+    workingDirectory: srcDir.path,
   );
   print(proResult.stdout);
   print(proResult.stderr);
@@ -41,5 +37,5 @@ void main(List<String> args) async {
   final libName = buildConfig.targetOs.libraryFileName(packageName, linkMode);
   final libUri = buildConfig.outDir.resolve(libName);
   print('libUri=$libUri');
-  File(p.join('src', _repoLibName)).renameSync(libUri.path);
+  File(p.join(srcDir.path, _repoLibName)).renameSync(libUri.path);
 }
