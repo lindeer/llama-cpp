710c4404466053cb0e18436058d5e393c8d826d0
lindeer <cljs118@126.com>
2024-01-05 19:50:28 +0800

    add example

---
+++ example/bin/llama_cpp_app.dart
@@ -0,0 +1,5 @@
+import 'package:llama_cpp/llama_cpp.dart' as llama_cpp;
+
+void main(List<String> arguments) {
+  llama_cpp.loadLlama('/home/wesley/Work/projects/chatgpt/models/grapewheel-7b-plus-ggml-q5_1.bin');
+}
