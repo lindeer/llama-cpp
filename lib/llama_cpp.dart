89f1738cdfbe7605c86d4a8de731d416d4156ee6
lindeer <cljs118@126.com>
2024-01-17 21:06:29 +0800

    usage of llama.cpp api

--- lib/llama_cpp.dart
+++ lib/llama_cpp.dart
@@ -13,7 +13,17 @@ final _libPath = Platform.isMacOS ? 'lib$_libBaseName.dylib'
 // final dylib = ffi.DynamicLibrary.open(_libPath);
 
 void loadLlama(String path) {
+  llama_cpp.llama_backend_init(false);
   final charPointer = path.toNativeUtf8().cast<ffi.Char>();
   final params = llama_cpp.llama_model_default_params();
-  final _model = llama_cpp.llama_load_model_from_file(charPointer, params);
+  final model = llama_cpp.llama_load_model_from_file(charPointer, params);
+  final ctxParams = llama_cpp.llama_context_default_params();
+  final ctx = llama_cpp.llama_new_context_with_model(model, ctxParams);
+
+  final contextSize = ctxParams.n_ctx;
+  final batchSize = ctxParams.n_batch;
+
+  llama_cpp.llama_free(ctx);
+  llama_cpp.llama_free_model(model);
+  llama_cpp.llama_backend_free();
 }
