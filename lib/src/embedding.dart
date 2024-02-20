b825f8eae11cdc63b3a257b5cec214c01ed910ef
lindeer <cljs118@126.com>
2024-02-20 16:49:44 +0800

    embedding implementation

---
+++ lib/src/embedding.dart
@@ -0,0 +1,116 @@
+import 'dart:ffi' as ffi;
+
+import 'package:ffi/ffi.dart' show calloc;
+import 'package:llama_cpp/native_llama_cpp.dart' as llama_cpp;
+
+import 'common.dart' as c;
+import 'ffi.dart';
+import 'llama_params.dart';
+
+/// Embedding runs in current isolate.
+/// Place it in another isolate if you want async embeddings.
+final class Embedding {
+  final ffi.Pointer<llama_cpp.llama_model> model;
+  final ffi.Pointer<llama_cpp.llama_context> ctx;
+  final NativeString cStr;
+  final bool verbose;
+  final tokenBuf = TokenArray(size: 64);
+
+  Embedding._(
+    this.model,
+    this.ctx,
+    this.cStr,
+    this.verbose,
+  );
+
+  factory Embedding(
+    String path, {
+    int? nThread,
+    int? nThreadBatch,
+    int? nCtx,
+    int? nBatch,
+    int? nGpuLayers,
+    bool verbose = false,
+  }) {
+    final cStr = NativeString();
+    path.into(cStr);
+    final (model, ctx) = c.loadModel(
+      cStr,
+      LlamaParams(
+        nThread: nThread,
+        nThreadBatch: nThreadBatch,
+        nCtx: nCtx,
+        nBatch: nBatch,
+        nGpuLayers: nGpuLayers,
+        embedding: true,
+      ),
+    );
+
+    return Embedding._(
+      model,
+      ctx,
+      cStr,
+      verbose,
+    );
+  }
+
+  List<List<double>> embedBatch(List<String> prompts) {
+    llama_cpp.llama_reset_timings(ctx);
+
+    final batchSize = llama_cpp.llama_n_batch(ctx);
+    final batch = llama_cpp.llama_batch_init(batchSize, 0, prompts.length);
+    final arrayList = prompts.map((p) {
+      p.into(cStr);
+      tokenBuf.pavedBy(model, cStr, addBos: true);
+      final l = tokenBuf.toList();
+      return l.length > batchSize ? l.sublist(0, batchSize) : l;
+    });
+    if (verbose) {
+      for (final (i, l) in arrayList.indexed) {
+        print("main: prompt $i: '${prompts[i]}'");
+        print("main: number of tokens in prompt = ${l.length}");
+        for (final t in l) {
+          print("${'$t'.padLeft(6)} -> '${cStr.tokenString(model, t)}'");
+        }
+      }
+    }
+
+    final dimens = llama_cpp.llama_n_embd(model);
+    final row = arrayList.length;
+    final data =
+        calloc.allocate<ffi.Float>(ffi.sizeOf<ffi.Float>() * row * dimens);
+    var out = data;
+    var s = 0;
+    for (final tokens in arrayList) {
+      final len = tokens.length;
+      if (batch.n_tokens + len > batchSize) {
+        c.decodeEmbeddingBatch(ctx, batch, out, s, dimens);
+        batch.n_tokens = 0;
+        out += s * dimens;
+        s = 0;
+      }
+      c.addBatchSeq(batch, tokens, s);
+      s++;
+    }
+    c.decodeEmbeddingBatch(ctx, batch, out, s, dimens);
+
+    final result = List<List<double>>.generate(row, (r) {
+      final p = data + r * dimens;
+      return List<double>.generate(dimens, (i) => p[i], growable: false);
+    }, growable: false);
+    llama_cpp.llama_print_timings(ctx);
+
+    calloc.free(data);
+    llama_cpp.llama_batch_free(batch);
+    return result;
+  }
+
+  void dispose() {
+    tokenBuf.dispose();
+    cStr.dispose();
+
+    llama_cpp.llama_free(ctx);
+    llama_cpp.llama_free_model(model);
+    llama_cpp.llama_backend_free();
+  }
+}
