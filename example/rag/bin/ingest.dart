ad99744b5fe5d9848266feb700b9da41f1c98c28
lindeer <cljs118@126.com>
2024-02-23 18:06:17 +0800

    RAG: ingest

--- example/rag/bin/ingest.dart
+++ example/rag/bin/ingest.dart
@@ -1,4 +1,28 @@
+import 'dart:convert' show json;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:rag/chroma.dart';
+import 'package:rag/common.dart' as c;
+
+List<ChromaDoc> _processDocuments(String dir, List<String> ignored) {
+  return [];
+}
+
+void main(List<String> argv) async {
+  final uri = Directory.current.uri.resolve('config.json');
+  final config = json.decode(await File.fromUri(uri).readAsString());
+
+  final chroma = await c.setupChroma(config);
+  final all = await chroma.allItems;
+  final ignored = all
+      .map((d) => d.metadata?['source'] as String?)
+      .whereType<String>()
+      .toList(growable: false);
+  final dir = config['source_dir'] ?? 'sources';
+
+  final at = DateTime.now().millisecondsSinceEpoch;
+  final docs = _processDocuments(dir, ignored);
+  await chroma.add(docs);
+  final cost = DateTime.now().millisecondsSinceEpoch - at;
+  print("${docs.length} documents cost $cost ms.");
 }
